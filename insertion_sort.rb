727cde1ef635e75aad8819e56ee92b95f0c6c1f5
vishaltiwari5529 <committed2win@gmail.com>
2016-04-09 16:07:00 +0000

    code-cleaning

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,23 +1,14 @@
 def insertion_sort(arr)
   len=arr.length
-  count=0
   for i in 0...len-1
     j=i+1
     while (arr[j-1]>arr[j] && j>0)
       swap(arr,j-1,j)
-      count+=1
       j-=1
     end
   end
   return arr,count
 end
-      
-      
-=begin       
-      func(a,b)
-      func a b
-      funct (a,b) #this throws arguement error
-=end
 
 def swap(arr,i,j)
   temp=arr[i]
